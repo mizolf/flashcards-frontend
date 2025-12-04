14887589dc024d8560e8cc1f7e7d52780a808e19
Mislav Češnik <mislav.cesnik88@gmail.com>
2025-12-04 18:44:30 +0100

    Add login/register page with registration toggle, install primeng ui component library v18.0.2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  isLoginMode = true;
+  showVerification = false;
+  public value: string | undefined;
 
+  toggleMode(mode: 'login' | 'register'): void {
+    this.isLoginMode = mode === 'login';
+    this.showVerification = false;
+  }
 }
