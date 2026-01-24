aa76628046e4514f0ba9df2d0798cfa9e3bcadff
filipjosipbionda <filip.josip.bionda@gmail.com>
2026-01-24 18:47:24 +0100

    WIP: add new deck

--- src/app/pages/my-decks/my-decks.component.ts
+++ src/app/pages/my-decks/my-decks.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+import { CreateDeckRequest, DeckResponse } from '../../models/Deck.dto';
+import { DeckService } from '../../services/deck.service';
+import { DeckFormDialogComponent } from '../../shared/deck-form-dialog/deck-form-dialog.component';
 
 @Component({
   selector: 'app-my-decks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonModule, DeckFormDialogComponent],
   templateUrl: './my-decks.component.html',
   styleUrl: './my-decks.component.scss'
 })
-export class MyDecksComponent {
+export class MyDecksComponent implements OnInit {
+  showCreateDialog = false;
+  decks: DeckResponse[] = [];
+  loading = false;
 
+  constructor(private deckService: DeckService) {}
+
+  ngOnInit(): void {
+    this.loadDecks();
+  }
+
+  openCreateDialog(): void {
+    this.showCreateDialog = true;
+  }
+
+  createDeck(request: CreateDeckRequest): void {
+    console.log('MyDecksComponent.createDeck: request', request);
+    this.deckService.createDeck(request).subscribe({
+      next: () => {
+        this.showCreateDialog = false;
+        // Response is empty; re-fetch from server.
+       // this.loadDecks();
+      },
+      error: (err) => {
+        console.error('Create deck failed', err);
+      }
+    });
+  }
+
+  private loadDecks(): void {
+    this.loading = true;
+    this.deckService.getDecks().subscribe({
+      next: (decks) => {
+        this.decks = decks;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
 }
--- src/app/shared/deck-form-dialog/deck-form-dialog.component.ts
+++ src/app/shared/deck-form-dialog/deck-form-dialog.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { CreateDeckRequest, DeckResponse } from '../../models/Deck.dto';
 
 @Component({
   selector: 'app-deck-form-dialog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ButtonModule, CheckboxModule, DialogModule, InputTextModule],
   templateUrl: './deck-form-dialog.component.html',
   styleUrl: './deck-form-dialog.component.scss'
 })
-export class DeckFormDialogComponent {
+export class DeckFormDialogComponent implements OnChanges {
+  @Input() visible: boolean = false;
+  @Input() deck: DeckResponse | null = null;
+  @Output() visibleChange = new EventEmitter<boolean>();
+  @Output() save = new EventEmitter<CreateDeckRequest>();
 
+  name = '';
+  isPublic = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['deck']) {
+      if (this.deck) {
+        this.name = this.deck.name ?? '';
+        this.isPublic = !!this.deck.isPublic;
+      } else {
+        this.resetForm();
+      }
+    }
+
+    if (changes['visible'] && this.visible && !this.deck) {
+      this.resetForm();
+    }
+  }
+
+  close(): void {
+    this.visibleChange.emit(false);
+  }
+
+  submit(): void {
+    const trimmedName = this.name.trim();
+    if (!trimmedName) {
+      console.warn('DeckFormDialogComponent.submit: name is empty');
+      return;
+    }
+
+    console.log('DeckFormDialogComponent.submit: emitting save', {
+      name: trimmedName,
+      isPublic: this.isPublic
+    });
+    this.save.emit({ name: trimmedName, isPublic: this.isPublic });
+  }
+
+  private resetForm(): void {
+    this.name = '';
+    this.isPublic = false;
+  }
 }
