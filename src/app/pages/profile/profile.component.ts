03cc785098e205b59f47c058c6ae90e35f7060cb
filipjosipbionda <filip.josip.bionda@gmail.com>
2026-01-26 19:37:27 +0100

    Implement profile screen

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { forkJoin } from 'rxjs';
+
+import { AuthenticationService } from '../../services/authentication.service';
+import { DeckService } from '../../services/deck.service';
+import { User } from '../../models/User.dto';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user$: Observable<User | null>;
+  loading = true;
+  isAuthenticated = true;
+  deckCount = 0;
+  cardCount = 0;
 
+  constructor(
+    private authService: AuthenticationService,
+    private deckService: DeckService
+  ) {
+    this.user$ = this.authService.currentUser;
+  }
+
+  ngOnInit(): void {
+    this.loading = true;
+    forkJoin({
+      isAuthenticated: this.authService.checkAuthStatus(),
+      decks: this.deckService.getDecks()
+    }).subscribe({
+      next: ({ isAuthenticated, decks }) => {
+        this.isAuthenticated = isAuthenticated;
+        this.deckCount = decks.length;
+        this.cardCount = decks.reduce((total, deck) => total + deck.cardCount, 0);
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
 }
